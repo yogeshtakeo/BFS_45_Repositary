f6c85edb62aa1f13ebc29aacf881732ed0144a20
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-02-28 12:42:43 -0600

    task 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./assets/styles.css";
+
+import image from "./assets/image.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="page">
+        <div className="center">
+          <div className="box">
+            <h1>Hello, World</h1>
+            <small>Work by Satshree Shrestha</small>
+          </div>
+        </div>
+        <br />
+        <div className="divider">
+          <hr />
+        </div>
+        <div className="center">
+          <img className="image" src={image} alt="image" />
+          <button className="btn" onClick={() => window.alert("HI! 👋🏻")}>
+            Click Me!
+          </button>
+          <a
+            className="link"
+            href="https://www.youtube.com/watch?v=28Lj4qt3lCc&pp=ygUSYWJvdXQgeW91IHRoZSAxOTc1"
+            target="_blank"
+          >
+            One of my favorite song
+          </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
