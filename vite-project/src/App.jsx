08e2b3f181bdc82fad67a4f4cefe93d4617c2bf6
Aakash <officialaakash97@gmail.com>
2024-02-27 21:20:58 -0500

    Make changes to the project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        CLICK ON THE VITE & REACT lOGOS TO LEARN MORE
       </p>
     </>
   )
