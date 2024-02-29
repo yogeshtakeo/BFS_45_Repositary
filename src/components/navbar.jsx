d562d81c1da14697c2df015ad66b755661c4d490
Nima Sherpa <sherpa2025@gmail.com>
2024-02-28 21:17:00 -0700

    Task2 CSS

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+
+function Navbar() {
+  return (
+    <div className="outerContainer">
+        <div className="navbar">
+            <a className="active" href="#">HOME</a>   
+            <a href="#todo">PROGRESS</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
