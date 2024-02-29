30723eb0c1dccfda844e53500f67013e4e1ce833
aashirwad43 <aashirwad43@gmail.com>
2024-02-29 03:37:25 -0600

    add takeo logo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import style from "../styles/components/login.module.css";
 import Image from "../components/image";
 import InputBox from "../components/inputBox";
 import Button from "../components/button";
+import takeo from "../assets/takeo-seo.png";
 
 const message1 = "Welcome Back!";
 const message2 = "Login to Continue";
@@ -9,9 +10,16 @@ const message2 = "Login to Continue";
 const placeholder1 = "Enter Username";
 const placeholder2 = "Enter Password";
 
+const takeoLink = "https://www.takeo.ai";
+
 function Login() {
   return (
-    <div>
+    <div className={style.outerDiv}>
+      <a href={takeoLink} target="_blank">
+        <div className={style.logoDiv}>
+          <img src={takeo} className={style.takeoImg} />
+        </div>
+      </a>
       <div className={style.row}>
         <div className={style.imageColumn}>
           <Image />
