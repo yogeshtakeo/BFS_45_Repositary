5e620f70ced23290c3f187235ef83037241ca05b
Nima Sherpa <sherpa2025@gmail.com>
2024-03-01 08:47:07 -0700

    stagger animation for new page

--- src/components/feature.jsx
+++ src/components/feature.jsx
@@ -2,26 +2,22 @@
 function Feature() {
   return (
     <>
-<div className="feature-container">
-        <div className="feature-box box1">
+      <div className="feature-container">
+        <div className="feature-box">
           <div className="text-overlay">
             <h2>PERTEX FABRICS</h2>
           </div>
         </div>
-        <div className="feature-box box2">
+        <div className="feature-box">
           <div className="text-overlay">
             <h2>YKK zippers</h2>
           </div>
         </div>
-        <div className="feature-box box3">
+        <div className="feature-box">
           <div className="text-overlay">
             <h2>900 fil power RDS</h2>
           </div>
         </div>
-      </div>
-
-      <div>
-        
       </div>
     </>
   )
