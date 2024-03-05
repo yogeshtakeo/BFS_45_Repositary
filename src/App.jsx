e9bece919ad31e846f0e315e30ba606560682bba
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-05 13:49:22 -0600

    set routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header/Header";
+import TaskList from "./components/TaskList/TaskList";
 
 function App() {
+  const [todoLists, updateList] = useState([]);
+
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <>
+          <Header />
+          <div className="page">
+            <Routes>
+              <Route
+                path="/"
+                exact={true}
+                element={
+                  <TaskList todoLists={todoLists} updateList={updateList} />
+                }
+              />
+            </Routes>
+          </div>
+        </>
+      </BrowserRouter>
     </>
   );
 }
