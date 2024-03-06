0144793c7bb82598ea3465a0bdafa33501c49c62
bik-on-git <bikash.me.shrestha@gmail.com>
2024-03-06 07:16:19 -0800

    to do list

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,70 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log(count);
+  const [newItem, setNewItem] = useState("");
+
+  const [items, setItems] = useState([]);
+
+
+  // add item function
+  function addItem() {
+    if (!newItem) {
+      alert("Enter the item");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+    };
+
+    setItems((oldList) => [...oldList, item]);
+    setNewItem("");
+  }
+
+
+
+  // delete item function
+  function deleteItem(id) {
+
+    const newArray = items.filter(item => item.id != id);
+    setItems(newArray);
+
+  }
+
+
   return (
-    <div className="container">
-      <button className="subtract" onClick={() => setCount(count - 1)}>
-        {" "}
-        Subtract 1{" "}
-      </button>
-      <p>{count}</p>
-      <button className="add" onClick={() => setCount(count + 1)}>
-        {" "}
-        Add 1{" "}
-      </button>
+    <div className="body" >
+      {/* 1. Header  */}
+      <h1 className="title"> Grocery List App</h1>
+
+
+
+      {/* 2. Input(input and Button) */}
+      <input
+        className="inputField"
+        type="text"
+        placeholder="Add an item"
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)
+
+        }
+      />
+
+      <button className="addButton" onClick={() => addItem()}>Add</button>
+
+
+
+      {/* 3. List of unordered items */}
+      <h3>Cart</h3>
+      <ul className="todoList">
+        {items.map((item) => {
+          return <li key={item.id} className="todoItem">{item.value}
+            <button className="deleteButton" onClick={() => deleteItem(item.id)}>❎</button>
+
+          </li>;
+        })}
+      </ul>
     </div>
   );
 }
